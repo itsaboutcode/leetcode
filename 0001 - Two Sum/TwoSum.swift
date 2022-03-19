359da1e9baef017fca755b4ec9bb88e37b8e8950
Muhammad Adil <adil@itsaboutcode.com>
2022-03-19 17:48:15 +0500

    First possible solution - simple

--- 0001 - Two Sum/TwoSum.swift
+++ 0001 - Two Sum/TwoSum.swift
@@ -1,1 +1,20 @@
-// https://leetcode.com/problems/two-sum/
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        var indices: [Int] = []
+        
+        for i in 0...nums.count-2 {
+            for j in i+1...nums.count-1 {
+                
+                if (nums[i] + nums[j] == target) {
+                    indices.append(i)
+                    indices.append(j)
+                    return indices
+                }
+                
+            }
+        }
+        
+        return indices; 
+    }
+}
